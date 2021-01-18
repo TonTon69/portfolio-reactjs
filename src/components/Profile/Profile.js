773ba982575564ba8490034c6e0420d30a9429f5
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2021-01-18 22:47:55 +0700

    Adding timeline to profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,38 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import CustomTimeline, { CustomTimelineSeparator } from "../Timeline/Timeline";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import resumeData from "../../utils/resumeData";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineContent from "@material-ui/lab/TimelineContent";
 import "./Profile.css";
 
-import CustomTimeline from "../Timeline/Timeline";
+const CustomTimelineItem = ({ title, text, link }) => (
+  <TimelineItem>
+    <CustomTimelineSeparator />
+    <TimelineContent className="timeline_content">
+      {link ? (
+        <Typography className="timelineItem_text">
+          <span>{title}:</span>{" "}
+          <a href={link} target="_blank">
+            {text}
+          </a>
+        </Typography>
+      ) : (
+        <Typography className="timelineItem_text">
+          <span>{title}:</span> {text}
+        </Typography>
+      )}
+    </TimelineContent>
+  </TimelineItem>
+);
 
 const Profile = () => {
   return (
     <div className="profile container_shadow">
       <div className="profile_name">
-        <Typography className="name">huynh minh hoang</Typography>
-        <Typography className="title">Web Developer</Typography>
+        <Typography className="name">{resumeData.name}</Typography>
+        <Typography className="title">{resumeData.title}</Typography>
       </div>
 
       <figure className="profile_image">
@@ -17,7 +40,20 @@ const Profile = () => {
       </figure>
 
       <div className="profile_information">
-        <CustomTimeline />
+        <CustomTimeline icon={<PersonOutlineIcon />}>
+          <CustomTimelineItem title="Name" text={resumeData.name} />
+          <CustomTimelineItem title="Birthday" text={resumeData.birthday} />
+          <CustomTimelineItem title="Phone" text={resumeData.phone} />
+          <CustomTimelineItem title="Email" text={resumeData.email} />
+
+          {Object.keys(resumeData.socials).map((key) => (
+            <CustomTimelineItem
+              title={key}
+              text={resumeData.socials[key].text}
+              link={resumeData.socials[key].link}
+            />
+          ))}
+        </CustomTimeline>
         <br />
         <button>btn</button>
       </div>
---
+++ src/utils/resumeData.js
@@ -0,0 +1,19 @@
+import React from "react";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+export default {
+  name: "Huynh Minh Hoang",
+  title: "Web Developer",
+  birthday: "06 September 2000",
+  email: "hoanghuynh0609@gmail.com",
+  address: "District 9, HCM City",
+  phone: "039 8412 102",
+
+  socials: {
+    GitHub: {
+      link: "https://github.com/tonton69",
+      text: "TonTon69",
+      icon: <GitHubIcon />,
+    },
+  },
+};

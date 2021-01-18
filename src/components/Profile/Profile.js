bc4650e87082e9d73f0879054e862d071f3b71dc
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2021-01-18 19:47:07 +0700

    Profile Component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import "./Profile.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">huynh minh hoang</Typography>
+        <Typography className="title">Web Developer</Typography>
+      </div>
+
+      <figure className="profile_image">
+        <img src={require("../../../src/assets/images/h.jpg").default} />
+      </figure>
+
+      <div className="profile_information">
+        Timeline information
+        <br />
+        <button>btn</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

62802e407d027cc547dc555ba7e9b529a6757f50
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2021-01-19 16:33:20 +0700

    Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import resumeData from "../../utils/resumeData";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <Typography className="footer_name">{resumeData.name}</Typography>
+      </div>
+      <div className="footer_right">
+        <Typography className="footer_copyright">
+          &copy; 2021. Designed and Developed by{" "}
+          <a href="#" target="_blank">
+            TonTon
+          </a>
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
